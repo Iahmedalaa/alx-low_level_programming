e63044444df3651b415b3b9ff6ebb5124886981f
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-08 16:01:38 -0700

    fdf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+int i;
+	for (i = 0; i < 9; i++)
+{
+	printf("%i\n", i);
+}
+	return (0);
+}
