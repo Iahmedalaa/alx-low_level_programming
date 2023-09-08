7e4b2577607c05e6ca7115da61e24a750b1bfee7
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-07 19:47:26 -0700

    dhkh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A c programm that prints with printf function
+ * Return value : Always 0 (success)
+*/
+
+int main(void)  
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
