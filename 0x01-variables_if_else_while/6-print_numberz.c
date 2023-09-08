64e94dc1ceebc22960eb1be289e9450c16bf6ed2
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-08 16:09:27 -0700

    fdhh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+int i;
+	for (i = 0; i < 10; i++)
+{
+	printf("%i\n", i);
+}
+	printf("\n");
+	return (0);
+}
