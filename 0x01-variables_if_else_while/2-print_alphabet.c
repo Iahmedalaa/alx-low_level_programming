199cd7d7e665c77f5a04de7abaa48421b92ae0cf
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-08 15:19:43 -0700

    fhd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (char <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
