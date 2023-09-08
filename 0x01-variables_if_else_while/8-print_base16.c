f6f01912ab302e3793848f06e29e3e25431d1dc4
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-08 16:48:01 -0700

    dsf

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+int digit = 0;
+char ch = 'a';
+	while (digit <= 9)
+{
+	putchar(digit + '0');
+	digit++;
+}
+	while (ch <= f)
+{
+	putchar(ch);
+	ch++;
+}
+	putchar('\n');
+
+	return (0);
+}
