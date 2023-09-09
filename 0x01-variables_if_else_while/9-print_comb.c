c52aa01aba5dbaaef9b8310ef1eff90f35c93610
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-08 17:06:10 -0700

    djlk

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+{
+	putchar(num + '0');
+
+	if (num != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+	num++;
+}
+	putchar('\n');
+
+	return (0);
