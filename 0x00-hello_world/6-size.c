7c802d0f542b15296937e72ced05f75ac7a9bc85
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-09 07:54:03 -0700

    FFF

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description : A c programm that prints with put function
- * return : always 0 (success)
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*
 */
 
 int main(void)
 {
-	printf("The size of char is: %ld byte(s)\n", sizeof(char));
-	printf("The size of int is: %ld byte(s)\n", sizeof(int));
-	printf("The size of long int is: %ld byte(s)\n", sizeof(long int));
-	printf("The size of long long is: %ld byte(s)\n", sizeof(long long));
-	printf("The size of long long int is: %ld byte(s)\n", sizeof(long long int));
-	printf("The size of float is: %ld byte(s)\n", sizeof(float));
+	printf("size of char: %ld byte(s)\n", sizeof(char));
+	printf("size of int: %ld byte(s)\n", sizeof(int));
+	printf("size of long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of long long: %ld byte(s)\n", sizeof(long long));
+	printf("size of long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of float: %ld byte(s)\n", sizeof(float));
+
 	return (0);
 }
