1c53fe4ee8fde67ae3f98e2298448c3fe057ec90
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-12 14:37:47 -0700

    HIfd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*/
+	void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+
+	void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+{
+	putchar('ch');
+
+	ch++;
+}
+	putchar('\n');
+
+
+}
