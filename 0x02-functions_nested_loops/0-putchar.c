48c4e60947a82f05fac67f1afb1ee09dbd41a604
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-12 11:19:58 -0700

    kfg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: show alphapet
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('/n');
+
+	return (0);
+
+}
