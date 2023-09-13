ae496102cc71d1673785da0854f4eb85b717cad8
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-13 06:42:37 -0700

    jk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _abs - function that will do sth
+*
+* @c: takes ascii
+*
+* Return: returns 1 if `c` is lowercase
+*       otherwise always 0 (success)
+*/
+
+	int _abs(int c)
+{
+	r = _abs(int)
+	putchar(r);
+
+}
