3c2f1bc30f87bf872d4c80c85fea27bf8d0e0af6
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-12 17:29:36 -0700

    jk

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+* print_sign - function that will do sth
+*
+* @n: takes intger
+*
+* Return: returns 1 if +  is lowercase
+*       otherwise always 0 (success)
+*/
+
+int print_sign(int n);
+{
+	if (n > o)
+	putchar('+');
+	return (1);
+	if (n == 0)
+	putchar('0');
+	return (0);
+	if (n < 0)
+	putchar('-1');
+	return (-1);
+
+
+}
