99d3bebd8e8d2343b14d03450b1b04bdb6367db5
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-13 08:24:20 -0700

    kjhj

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_last_digit - function that will do sth
+*
+* @c: takes ascii
+*
+* Return: returns 1 if `c` is lowercase
+*       otherwise always 0 (success)
+*/
+
+	int print_last_digit(int c)
+{
+	num = 10%c;
+	return (num);
+}
