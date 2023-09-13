016d93ec32e9915964afc59f1f04c9046951f8c1
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-13 10:24:28 -0700

    f

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,9 @@ void print_to_98(int n)
 
 	if (n > 98)
 	for (i = n; i > 98; i--)
-	printf("%d, ".i);
+	printf("%d, ", i);
 	else
 	for (i = n; i < 98; i++)
-	printf("%d, ".i);
-	printf("98\n);
+	printf("%d, ", i);
+	printf("98\n");
 }
