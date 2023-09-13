7e9e3d8a86a6138a1228c130097801703bc42834
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-13 10:12:45 -0700

    dFJk

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* add - function that will do sth
+* @a: fgkdj
+* @b: kfds
+*
+* Return:jhkgkjfd
+*/
+
+int add(int b, int a)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
