485d5cf94ffe49bc90d84f12996c31c65dd5fc85
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-18 17:22:40 -0700

    lkfj

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+* rev_string - function that will do sth
+*
+* @s: talke
+* Return: returns 1 if +  is lowercase
+*       otherwise always 0 (success)
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	i++;
+	while (i--)
+{
+	putchar(s[i]);
+}
+}
