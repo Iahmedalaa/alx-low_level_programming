b3837dbcace41c1e5b6914fd2a0e7b958e808d50
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-21 14:39:11 -0700

    dlk

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,9 +15,12 @@ char *_strncat(char *dest, char *src, int n)
 int c, c2;
 
 	c = 0;
+	/* djkd fhdh dkd*/
 	while (dest[c])
 	c++;
+	/* fdjld dldkl*/
 	for (c2 = 0; src[c2] ; c2++)
+	/* dlj dkl */
 	dest[c++] = src[c2];
 
 	return (dest);
