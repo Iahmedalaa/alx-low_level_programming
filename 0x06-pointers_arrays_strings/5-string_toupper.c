97f72232aa8312d8ff9e9143336d683c83573907
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-21 16:51:58 -0700

    hdk

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+* *string_toupper - function that will do sth
+*
+* @str: talke
+* Return: returns 1 if +  is lowercase
+*       otherwise always 0 (success)
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0''; i++)
+{
+	if (str[i] >= 97 && str[i] <= 122)
+{
+	str[i] = str[i] -32;
+}
+}
+	return (str);
+}
