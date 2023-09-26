af6d10b6daa6b23e563c2307006f43397645ba40
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-25 18:38:25 -0700

    kdKJ

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* set_string - function that will do
+* @to: kds
+* @s: dl
+* Return: returns 1 if +  is lowercase
+*       otherwise always 0 (success)
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
