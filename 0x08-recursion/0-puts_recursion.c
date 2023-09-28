df0e3c6d78d0d0b29ac21c2cd7cfe5ac258fbc91
Iahmedalaa <iahmedalaa430@gmail.com>
2023-09-28 16:17:02 -0700

    kld

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+* _puts_recursion - function that will do
+* @s: kds
+*/
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+{
+	putchar ('\n');
+}
+	else
+{
+	putchar (*s);
+	_puts_recursion(s + 1);
+}
+}
