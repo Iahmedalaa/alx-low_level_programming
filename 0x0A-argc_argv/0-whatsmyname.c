ce91b04677d08cb3343705bb1f8a71532c581838
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-02 16:50:56 -0700

    dlk

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+/**
+ * main - do sth dl. 
+ * @argc: dkd 
+ * @argv: kd 
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
