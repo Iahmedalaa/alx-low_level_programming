eef32c7d001d9bcd0116f64b32c060c60849ef2b
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-02 17:01:01 -0700

    dlkd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - do sth dl.
+ * @argc: dkd
+ * @argv: kd
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
