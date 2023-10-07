39a015abbb0702df8f65682bfa94ef0131e0791b
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-07 01:23:47 -0700

    kdksjk

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *array_range - do sth dl.
+ * @max: dkd
+ * @min: kd
+ * Return: 0
+ */
+
+int *array_range(int min, int max)
+{
+	int i, len;
+	int *ptr;
+
+	if (min > max)
+	return (NULL);
+	len = max - min + 1;
+	ptr = malloc(sizeof(*int) * len);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
+
