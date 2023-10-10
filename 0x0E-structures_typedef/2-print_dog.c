69b378ac38fa134c03c935f09e3fb789ac546fc6
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-10 14:06:31 -0700

    lkd

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include"dog.h"
+
+/**
+ * print_dog - do sth dl.
+ * @name: dkd
+ * @age: kd
+ * @d: dka
+ * @owner:dddfr
+ *
+ * Description: dklsjk ldskjv l
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == '\0')
+	return (NULL);
+
+	struct dog
+	{
+	char name;
+	float age;
+	char owner;
+	}
+	dog my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+}
