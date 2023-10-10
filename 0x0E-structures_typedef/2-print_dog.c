c6d227900e4752a12338289d23d799b0d90fec42
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-10 14:15:01 -0700

    lkd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,27 +1,19 @@
 #include"dog.h"
+#include<stdio.h>
 
 /**
  * print_dog - do sth dl.
- * @name: dkd
- * @age: kd
  * @d: dka
- * @owner:dddfr
  *
  * Description: dklsjk ldskjv l
  */
 
 void print_dog(struct dog *d)
 {
-	if (d == '\0')
-	return (NULL);
-
-	struct dog
-	{
-	char name;
-	float age;
-	char owner;
-	}
-	dog my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
+	if (d)
+{
+	printf("Name: %s\n", d->name ? name : "(nil)");
+	printf("Name: %s\n", d->age);
+	printf("Name: %s\n", d->owner ? owner : "(nil)");
+}
 }
