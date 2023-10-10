11682d0609b3c0ca085911ccf6c7fc781ef758f2
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-10 13:57:37 -0700

    lkl

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include"dog.h"
+
+/**
+ * init_dog - do sth dl.
+ * @name: dkd
+ * @age: kd
+ * @d: dka
+ * @owner:dddfr
+ *
+ * Description: dklsjk ldskjv l
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
