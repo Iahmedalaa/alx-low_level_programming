0582a215afb73687ef3eb76b57112795025c4fa4
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-12 13:13:43 -0700

    jklfd

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - sments
+ * @n: the
+ * @...: the
+ * Return: the
+ */
+	int sum_them_all(const unsigned int n, ...)
+	}
+	int x = 0, v = n;
+	va_list ap;
+
+	if (!n)
+	return (0);
+	va_start(ap, n);
+	while (v--)
+	x += va_arg(ap, int);
+	va_end(ap);
+		return (x);
+	}
