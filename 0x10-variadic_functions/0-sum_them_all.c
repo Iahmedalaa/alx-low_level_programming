81b0657986f07729db7b0ff2c5aa05f8d3fad30c
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-12 13:56:56 -0700

    kldf

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,14 @@
  */
 	int sum_them_all(const unsigned int n, ...)
 {
-	int x = 0, v = n;
+	int s = 0, i = n;
 	va_list ap;
 
 	if (!n)
 	return (0);
 	va_start(ap, n);
-	while (v--)
-	x += va_arg(ap, int);
+	while (i--)
+	s += va_arg(ap, int);
 	va_end(ap);
-		return (x);
+		return (s);
 }
