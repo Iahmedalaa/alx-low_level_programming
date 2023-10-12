ffd0854babdd99017bf63c8a6ac616cd8599fde2
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-12 13:39:36 -0700

    jldsk

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - do sth dl.
+ * @separator: dkd
+ * @n: kd
+ * @..: lds skl
+ * Return: dsf d
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+
+	if (!n)
+{
+		printf("\n");
+	return;
+}
+	va_start(ap, n);
+	while (i--)
+	printf("%d%s", va_arg(ap, int),
+			i ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
