879cc6be0653c37dcd0299faacd7cef059f81c34
Iahmedalaa <iahmedalaa430@gmail.com>
2023-10-13 08:45:54 -0700

    dk

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,21 @@
-#include "variadic_functions.h"
-
+#include "variadic_funcions.h"
 /**
- *sum_them_all - sments
- * @n: the
- * @...: the
- * Return: the
+ * sum_them_all - jdll
+ * @n:jkds
+ * @...: teh
+ * Return: sld
  */
-	int sum_them_all(const unsigned int n, ...)
+
+int sum_them_all(const unsigned int n, ...)
 {
 	int s = 0, i = n;
 	va_list ap;
 
 	if (!n)
-	return (0);
+		return (0);
 	va_start(ap, n);
 	while (i--)
-	s += va_arg(ap, int);
+		s += va_arg(ap, int);
 	va_end(ap);
-		return (s);
+	return (s);
 }
