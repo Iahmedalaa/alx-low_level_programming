bc8208fe4383e3c8bd6ca0a6eeaedeed90df00fd
iahmedalaa <iahmedalaa439@gmail.com>
2023-10-19 09:35:36 -0700

    0-is done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_list - check the code for The school students.
+* @h: the number of ar
+* Return: size
+*/
+
+
+size_t print_list(const list_t *h)
+{
+	size_t z = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (z);
+}
